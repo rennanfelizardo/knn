c1a1902666bd5bc4e5678cfd3da20dd38564fdfa
Rennan Felizardo <rennan.felizardo@dce.ufpb.br>
2016-05-19 20:59:12 -0300

    Criação das classes Iris e DatasetIris

---
+++ src/DatasetIris.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class DatasetIris {
+
+	public static void main(String[] args) {
+		
+		try{
+			Scanner sc = new Scanner("treinamento.csv");
+			
+		} catch (Exception e){
+			
+		}
+		
+		
+		
+
+	}
+
+}
---
+++ src/Iris.java
@@ -0,0 +1,24 @@
+
+public class Iris {
+
+	double sepallength;
+	double sepalwidth;
+	double petallength;
+	double petalwidth;
+	int label;
+
+
+	public Iris (double sepallength, double sepalwidth, double petallength, double petalwidth, int label){
+		this.sepallength = sepallength;
+		this.sepalwidth = sepalwidth;
+		this.petallength = petallength;
+		this.petalwidth = petalwidth;
+		this.label = label;
+	}
+
+	public double calcDistancia(Iris iris){
+		
+	}
+			
+	
+}
